c5e633a1f433ab713294a03302bc6e3435b0fb6e
Ryan Pethel <arpethel@gmail.com>
2019-07-09 07:38:32 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -2,9 +2,6 @@ require 'pry'
 class Helpers
   def self.current_user(session)
     @user = User.find_by_id(session[:user_id])
-    # if session[:user_id] == @user.id
-    #   @user
-    # end
   end
 
   def self.is_logged_in?(session)
